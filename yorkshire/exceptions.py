16a0cacc498c2f183ee6d664f233b51e5b33820f
D Niu <d.niu@datadoghq.com>
2023-03-10 10:35:48 -0500

    improve remote url parsig

--- yorkshire/exceptions.py
+++ yorkshire/exceptions.py
@@ -15,3 +15,6 @@ class DownloadFileError(YorkshireException):
 
 class FileParseError(YorkshireException):
     """An exception raised when cannot parse the given requirements file."""
+
+class GithubRateLimitError(DownloadFileError):
+    """An exception was raised while downloading a remote requirments file because of rate limiting from Github."""
