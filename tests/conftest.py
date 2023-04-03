3a2dddb0f9022303f4d7df38fbd2cecf94a265de
Cédric Van Rompay <cedric.vanrompay@datadoghq.com>
2023-04-03 09:36:53 +0200

    comment about conftest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+# About this file, see
+# https://docs.pytest.org/en/6.2.x/writing_plugins.html#localplugin
+
 def pytest_generate_tests(metafunc):
     if "index_allowlist" in metafunc.fixturenames:
         metafunc.parametrize("index_allowlist", [False, True])
